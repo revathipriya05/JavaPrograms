da663f9d688d02b53d5b5a14b18271e069f34ab3
Revathi Priya B <168623660+revathipriya05@users.noreply.github.com>
2025-01-23 18:55:07 +0530

    Update Float.java

--- Float.java
+++ Float.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 class Float {
 public static void main(String[] args) {
-System.out.println("Enter the first number:");
 Scanner sc = new Scanner(System.in);
+System.out.println("Enter the first number:");
 float n1 = sc.nextFloat();
 System.out.println("Enter the second number:");
 float n2 = sc.nextFloat();
