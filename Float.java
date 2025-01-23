14bda4ac0aa766a41c7633b3e3f23db884adadcf
Revathi Priya B <168623660+revathipriya05@users.noreply.github.com>
2025-01-23 05:23:50 -0800

    Add files via upload

---
+++ Float.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Float {
+public static void main(String[] args) {
+System.out.println("Enter the first number:");
+Scanner sc = new Scanner(System.in);
+float n1 = sc.nextFloat();
+System.out.println("Enter the second number:");
+float n2 = sc.nextFloat();
+if((int)(n1*1000)==(int)(n2*1000))
+{
+System.out.println("The numbers are equal");
+}
+else
+{
+System.out.println("The numbers are not equal");
+}
+sc.close();
+}
+}
+
+
